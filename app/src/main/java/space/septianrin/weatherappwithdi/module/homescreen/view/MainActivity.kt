f6bf8199fffef308562e6c0a6d5d12e63b31e9c9
Septian Rinaldi Rianggoro <septian.rianggoro@ist.id>
2023-09-05 15:05:41 +0700

    add nunito font, layout the mainactivity, add expandable for more info when clicking the card

--- app/src/main/java/space/septianrin/weatherappwithdi/module/homescreen/view/MainActivity.kt
+++ app/src/main/java/space/septianrin/weatherappwithdi/module/homescreen/view/MainActivity.kt
@@ -3,6 +3,8 @@ package space.septianrin.weatherappwithdi.module.homescreen.view
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.animation.AnimationUtils
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
@@ -21,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+    private var isExpanded = false
 
     @Inject
     lateinit var apiKey: String
@@ -29,25 +32,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.randomizeWeather.setOnClickListener {
-            val city = weatherViewModel.getRandomizedCity()
-            weatherViewModel.getCityWeather(
-                city,
-                { response ->
-                    weatherViewModel.saveData(response)
-                }, { error ->
-                    Log.e("onCreate: ", error.toString())
-                })
-        }
-        weatherViewModel.weatherLiveData.observe(this) { weatherData ->
-            binding.weatherTextView.text =
-                "${weatherData.location.name} : ${weatherData.current.condition.text} => ${weatherData.current.tempCelsius}°C"
-            Glide.with(this)
-                .load(("http:" + weatherData.current.condition.icon).replace("64x64","128x128"))
-                .placeholder(R.drawable.ic_launcher_background)
-                .error(R.drawable.ic_launcher_background)
-                .into(binding.weatherImageView)
-        }
+        onListener()
+        onObserve()
+
         lifecycleScope.launch {
             weatherViewModel.getCityWeather(
                 "London",
@@ -57,5 +44,57 @@ class MainActivity : AppCompatActivity() {
                     Log.e("onCreate: ", error.toString())
                 })
         }
+    }
+
+    private fun onObserve() {
+        weatherViewModel.weatherLiveData.observe(this) { weatherData ->
+            with(binding){
+                cityNameTextView.text = weatherData.location.name
+                currentTemperature.text = "Current : ${weatherData.current.tempCelsius}°C"
+                feelLikeTemperature.text = "Feel Like : ${weatherData.current.feelslikeCelsius}°C"
+                valueUV.text = "${weatherData.current.uv}"
+                valueCloudPercent.text = "${weatherData.current.cloud}%"
+                valueHumidityPercent.text = "${weatherData.current.humidity}"
+                valueWindKph.text = "${weatherData.current.windKph}"
+                valueWindDirection.text = "${weatherData.current.windDirection}"
+
+                Glide.with(this@MainActivity)
+                    .load(("http:" + weatherData.current.condition.icon).replace("64x64","128x128"))
+                    .placeholder(R.drawable.ic_launcher_background)
+                    .error(R.drawable.ic_launcher_background)
+                    .into(weatherImageView)
+            }
+
+        }
+    }
+
+    private fun onListener() {
+        with(binding){
+            randomizeWeather.setOnClickListener {
+                val city = weatherViewModel.getRandomizedCity()
+                weatherViewModel.getCityWeather(
+                    city,
+                    { response ->
+                        weatherViewModel.saveData(response)
+                    }, { error ->
+                        Log.e("onCreate: ", error.toString())
+                    })
+            }
+
+            weatherCardView.setOnClickListener {
+                if (isExpanded) {
+                    // Collapse the expandable layout
+                    expandableLayout.startAnimation(AnimationUtils.loadAnimation(applicationContext, R.anim.collapse_animation));
+                    expandableLayout.visibility = View.GONE
+                    isExpanded = false
+                } else {
+                    // Expand the expandable layout
+                    expandableLayout.startAnimation(AnimationUtils.loadAnimation(applicationContext, R.anim.expand_animation));
+                    expandableLayout.visibility = View.VISIBLE
+                    isExpanded = true
+                }
+            }
+        }
+
     }
 }
